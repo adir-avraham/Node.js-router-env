a78cd0aad475b5c3794d3f5a5a30a200527ca631
adir-avraham <adir.avraham@gmail.com>
2019-11-11 19:15:15 +0200

    fix-class

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser')
 api = express();
-
+const moment = require("moment");
+const users = require('./users')
 const authRouter = require('./routes/auth')
+const flightsRoute = require('./routes/flights')
 
 api.use(bodyParser.json());
 
@@ -13,6 +15,17 @@ api.listen(process.env.PORT || 5000, () =>{
     console.log(`Api is listening to port: ${process.env.PORT}`)
 })
 
-
 api.use('/auth', authRouter )
-api.use('/flights', authRouter)
+
+api.use((req, res, next) => {
+    const { key } = req.query;
+    const timeStamp = moment().format("x");
+    if (!users[key]) return res.status(401).send("Unauthorized user");
+    if (timeStamp - 60000 > users[key]) {
+      delete users[key];
+      return res.send("Your key is expierd/Unauthorized user");
+    }
+    next();
+  });
+
+api.use('/flights', flightsRoute)
--- routes/auth.js
+++ routes/auth.js
@@ -1,52 +1,39 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const moment = require('moment')
-const users = {};
-const flightsRouter = require('./flights')
-const logger = require('../utils/logger')
+const moment = require("moment");
+const users = require('../users')
+const flightsRouter = require("./flights");
+const logger = require("../utils/logger");
 
+router.use("/Register", (req, res, next) => {
+  const { userName, email, password, firstName, lastName } = req.body;
+  if (!userName || !email || !password || !firstName || !lastName) {
+    res.send("Some value is missing...");
+  }
+  if (!email.includes("@")) return res.send("Email address is not valid");
+  next();
+});
 
+router.post("/Register", (req, res, next) => {
+  const apiKey = getApiKey();
+  const expiration = moment().format("x");
 
-router.use('/Register', (req, res, next) =>{
-    const {userName, email, password, firstName, lastName} = req.body;
-    if (!userName || !email || !password || !firstName || !lastName ) {
-        res.send("Some value is missing...")
-    }
-    if (!email.includes("@")) return res.send("Email address is not valid");
-    next()
-})
+  users[apiKey] = expiration;
 
-
-router.post('/Register', (req, res, next) =>{   
-    const apiKey = getApiKey()
-    const expiration = moment().format('x')
-    
-    users[apiKey] = expiration
-    
-    logger.info(`Api Key: ${apiKey} was given to a user `)
-    res.send(`Register success - your key is ${apiKey}`)
-})
-
+  logger.info(`Api Key: ${apiKey} was given to a user `);
+  res.send(`Register success - your key is ${apiKey}`);
+});
 
 function getApiKey() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
-router.use('/flights', (req, res, next)=>{
-    const {key} = req.query;
-    const timeStamp = moment().format('x')
-    if (!users[key]) return res.status(401).send("Unauthorized user")
-    if (timeStamp - 60000 > users[key]) {
-      delete users[key];
-      return res.send("Your key is expierd/Unauthorized user")
-    } 
-    next()
-})
 
-router.get('/flights', flightsRouter )
 
+//router.get("/flights", flightsRouter);
 
 module.exports = router;
