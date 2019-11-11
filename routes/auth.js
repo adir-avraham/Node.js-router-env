b5fd71df2d800c4666ec366efe4bb3dbb2e910ac
adir-avraham <adir.avraham@gmail.com>
2019-11-11 17:21:49 +0200

    fix-bug

--- routes/auth.js
+++ routes/auth.js
@@ -41,7 +41,7 @@ router.use('/flights', (req, res, next)=>{
     if (!users[key]) return res.status(401).send("Unauthorized user")
     if (timeStamp - 60000 > users[key]) {
       delete users[key];
-      return res.status(401).res.send("Your key is expierd/Unauthorized user")
+      return res.send("Your key is expierd/Unauthorized user")
     } 
     next()
 })
